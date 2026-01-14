b9e46b984eb1fb5bc961a7183457e3800a0f376b
methodeapro-commits <method.ea.pro@gmail.com>
2026-01-15 01:37:26 +0300

    без лого
    без лого

---
+++ config.js
@@ -0,0 +1,12 @@
+window.__scormifyConstants__ = {
+    slides: [{ src: 'slide_1.jpg' },{ src: 'slide_2.jpg' },{ src: 'slide_3.jpg' },{ src: 'slide_4.jpg' },{ src: 'slide_5.jpg' },{ src: 'slide_6.jpg' },{ src: 'slide_7.jpg' },{ src: 'slide_8.jpg' },{ src: 'slide_9.jpg' },{ src: 'slide_10.jpg' },{ src: 'slide_11.jpg' },{ src: 'slide_12.jpg' },{ src: 'slide_13.jpg' },{ src: 'slide_14.jpg' },{ src: 'slide_15.jpg' },{ src: 'slide_16.jpg' },{ src: 'slide_17.jpg' },{ src: 'slide_18.jpg' },{ src: 'slide_19.jpg' },{ src: 'slide_20.jpg' },{ src: 'slide_21.jpg' },{ src: 'slide_22.jpg' },{ src: 'slide_23.jpg' },{ src: 'slide_24.jpg' },{ src: 'slide_25.jpg' },{ src: 'slide_26.jpg' },{ src: 'slide_27.jpg' },{ src: 'slide_28.jpg' },{ src: 'slide_29.jpg' },{ src: 'slide_30.jpg' },{ src: 'slide_31.jpg' },{ src: 'slide_32.jpg' },{ src: 'slide_33.jpg' },{ src: 'slide_34.jpg' },{ src: 'slide_35.jpg' },{ src: 'slide_36.jpg' },{ src: 'slide_37.jpg' },{ src: 'slide_38.jpg' },{ src: 'slide_39.jpg' },{ src: 'slide_40.jpg' },{ src: 'slide_41.jpg' },{ src: 'slide_42.jpg' },{ src: 'slide_43.jpg' },{ src: 'slide_44.jpg' }],
+    threshold: 100,
+    allowLangOverride: true,
+    language: 'ru',
+    api: 'SCORM12',
+    forceWindowSize: false,
+    windowSize: {
+      height: 900,
+      width: 1475
+    }
+  };
